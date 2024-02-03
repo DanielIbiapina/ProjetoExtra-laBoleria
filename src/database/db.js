5a6e25aa55cfd27c7d953ff7e153ad47a4d80d59
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2024-02-03 12:27:41 -0300

    ssl

--- src/database/db.js
+++ src/database/db.js
@@ -2,8 +2,9 @@ import pkg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const {Pool} = pkg;
+const { Pool } = pkg;
 
 export const connectionDB = new Pool({
-    connectionString: process.env.DATABASE_URL,
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
